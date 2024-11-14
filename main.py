6def7a6756217a2208dd8f1ce2fcb6012679ba6c
AdamBrik <adam.brik@edu.esiee.fr>
2024-11-14 10:37:55 +0100

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 from math import sqrt
-
 #### Fonction secondaire
-
-
-def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
-
-
-def main():
-
-    # vos appels à la fonction secondaire ici
-
+def isprime(p): #Verifie si un nombre est premier
+    if p <= 1 :
+        return False
+    if p==2 :
+        return True 
+    if p%2 == 0 :
+        return False
+    for i in range(3,int(sqrt(p))+1,2) :
+        if p%i == 0:
+            return False
+    return True
+####Fonction principale
+def main(): # Fonction principale pour lister les nombres premiers jusqu'à 100
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
-
 if __name__ == "__main__":
     main()
