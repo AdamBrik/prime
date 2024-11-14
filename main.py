0b7cc3686bbf329915e5afc13ac7b539662ed9e8
AdamBrik <adam.brik@edu.esiee.fr>
2024-11-14 10:06:48 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
+"""#Module pour vérifier les nombres premiers et les lister jusqu'à une limite donnée."""
 from math import sqrt
 #### Fonction secondaire
 def isprime(p): #Verifie si un nombre est premier
-    if p <= 1 :
+    """Vérifie si un nombre est premier.
+
+    Argument: p est un nombre entier à vérifier.
+
+    Return: return True si p est un nombre premier sinon il retourne False
+    """
+    if p <= 1:
         return False
-    if p==2 :
-        return True 
-    if p%2 == 0 :
+    if p == 2:
+        return True
+    if p % 2 == 0:
         return False
-    for i in range(3,int(sqrt(p))+1,2) :
-        if p%i == 0:
+    for i in range(3, int(sqrt(p)) + 1, 2):
+        if p % i == 0:
             return False
     return True
 ####Fonction principale
-def main(): # Fonction principale pour lister les nombres premiers jusqu'à 100
+
+def main():
+    """Fonction principale pour lister les nombres premiers jusqu'à 100."""
+    # Appels à la fonction secondaire
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
     print()
+
 if __name__ == "__main__":
     main()
